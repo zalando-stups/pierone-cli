45a43ddfc788ab3f68f66479f1243a6506ed44a1
João Santos <joao@zalando.de>
2018-11-29 09:01:17 +0100

    Set minimum pytest version

--- setup.py
+++ setup.py
@@ -135,7 +135,7 @@ def setup_package():
         install_requires=install_reqs,
         setup_requires=['six', 'flake8'],
         cmdclass=cmdclass,
-        tests_require=['pytest-cov', 'pytest', "hypothesis"],
+        tests_require=['pytest-cov', 'pytest>=3.3', "hypothesis"],
         command_options=command_options,
         entry_points={'console_scripts': CONSOLE_SCRIPTS},
     )
